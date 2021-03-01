56562e9d5ee9df0b74c34945bc0167858fd3bd97
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-01 19:02:47 +0530

    [vijay]AppendMethodAdded

--- LinkedListDay16/Program.cs
+++ LinkedListDay16/Program.cs
@@ -10,16 +10,22 @@ namespace LinkedListDay16
         {
             Console.WriteLine("Welcome to Linked List");
             LinkedList list = new LinkedList();
+            //UC1
             list.InsertLast(56);
             list.InsertLast(30);
             list.InsertLast(70);
-
             list.Display();
-
+            //UC2
             list.InsertFront(70);
             list.InsertFront(30);
             list.InsertFront(56);
             list.Display();
+            //UC3
+            list.Append(56);
+            list.Append(30);
+            list.Append(70);
+            list.Display();
         }
+           
     }
 }
